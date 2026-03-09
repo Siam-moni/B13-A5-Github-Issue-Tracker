338018f8d82413c191c982bda53b3e1066790ed1
Siam-moni <shariars012@gmail.com>
2026-03-09 19:44:53 +0600

    Display issues in responsive card layout

--- script.js
+++ script.js
@@ -28,3 +28,113 @@ async function loadIssues() {
 
   loader.classList.add("hidden");
 }
+// DISPLAY ISSUES
+function displayIssues(issues) {
+  const container = document.getElementById("issuesContainer");
+  const count = document.getElementById("issueCount");
+
+  container.innerHTML = "";
+  count.innerText = issues.length + " Issues";
+
+  issues.forEach((issue) => {
+    const border =
+      issue.status === "open" ? "border-green-500" : "border-purple-500";
+
+    // LABEL DESIGN
+    let labelsHTML = "";
+
+    if (issue.labels && issue.labels.includes("bug")) {
+      labelsHTML += `
+      <span class="flex items-center gap-1 text-red-500 border border-red-300 px-3 py-1 rounded-full text-xs">
+        <img src="./assets/bug.png" alt="" class="w-4" /> BUG
+      </span>
+      `;
+    }
+
+    if (issue.labels && issue.labels.includes("help wanted")) {
+      labelsHTML += `
+      <span class="flex items-center gap-1 text-green-800 border border-yellow-400 px-3 py-1 rounded-full text-xs">
+        <img src="./assets/Open-Status.png" alt="" class="w-3" /> HELP WANTED
+      </span>
+      `;
+    }
+    if (issue.labels && issue.labels.includes("enhancement")) {
+      labelsHTML += `
+      <span class="flex items-center gap-1 text-red-500 border border-red-300 px-3 py-1 rounded-full text-xs">
+        <img src="./assets/enhance.png" alt="" class="w-3" />Enhancement
+      </span>
+      `;
+    }
+    if (issue.labels && issue.labels.includes("good first issue")) {
+      labelsHTML += `
+      <span class="flex items-center gap-1 text-green-500 border border-red-300 px-3 py-1 rounded-full text-xs">
+        <img src="./assets/good issues.png" alt="" class="w-3" /> Good Issue
+      </span>
+      `;
+    }
+    if (issue.labels && issue.labels.includes("documentation")) {
+      labelsHTML += `
+      <span class="flex items-center gap-1 text-black border border-red-300 px-3 py-1 rounded-full text-xs">
+        <img src="./assets/documention.png" alt="" class="w-3" /> documentation
+      </span>
+      `;
+    }
+    // STATUS ICON
+    let statusIconHTML =
+      issue.status === "open"
+        ? `<div class="rounded-full border border-[#22C55E] p-1">
+            <img src="./assets/Open-Status.png" alt="" class="w-4" />
+           </div>`
+        : `<div class="rounded-full border border-[#A855F7] p-1">
+            <img src="./assets/Closed- Status .png" alt="" class="w-4" />
+           </div>`;
+
+    const card = document.createElement("div");
+
+    const date = new Date(issue.createdAt);
+    const formattedDate =
+      date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+
+    card.className = `bg-white p-5 rounded-xl shadow border-t-4 ${border} cursor-pointer hover:shadow-lg transition`;
+
+    card.innerHTML = `
+    
+    <div class="flex justify-between items-center mb-3">
+
+        <span>${statusIconHTML}</span>
+
+        <span class="bg-red-100 text-red-500 px-3 py-1 rounded-full text-xs font-semibold">
+        ${issue.priority}
+        </span>
+
+    </div>
+
+    <h3 class="font-semibold text-lg">
+    ${issue.title}
+    </h3>
+
+    <p class="text-sm text-gray-500 mt-2">
+    ${issue.description}
+    </p>
+
+    <div class="flex gap-2 mt-3 flex-wrap">
+    ${labelsHTML}
+    </div>
+
+    <hr class="my-3">
+
+    <p class="text-xs text-gray-500">
+    #${issue.id} by ${issue.author}
+    </p>
+
+    <p class="text-xs text-gray-400">
+    
+    ${formattedDate}
+    </p>
+    `;
+
+    card.onclick = () => openModal(issue.id);
+
+    container.appendChild(card);
+  });
+}
